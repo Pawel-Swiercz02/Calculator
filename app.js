a5ff3341a0fa0d6fdd86f98b1c3bbe926b34ca2d
Paweł Świercz <150139537+Pawel-Swiercz02@users.noreply.github.com>
2024-01-24 13:50:47 +0100

    Add files via upload
    added firstnumber creation throught clicking the 1-9 buttons

--- app.js
+++ app.js
@@ -1,22 +1,84 @@
+const resultDisplay = document.querySelector('.bottom-result-display')
+const numberKeyArr = document.querySelectorAll('.number-key');
+let resultValue = 0;
+let firstNumber = '';
+let secondNumber = '';
+let operatorChosen = false;
 
 
-function add(firstNumber, secondNumber) {
-    return firstNumber + secondNumber;
-}
+resultDisplay.innerHTML = resultValue;
 
-function subtract(firstNumber, secondNumber) {
-    return firstNumber - secondNumber;
-}
+function numberButtons() {
+    numberKeyArr.forEach(numberKey => {
+        numberKey.addEventListener('click', changeNumber);
+    });
+};
+numberButtons();
 
-function multiply(firstNumber, secondNumber) {
-    return firstNumber * secondNumber;
-}
+function changeNumber() {
+    if ((firstNumber.toString()).length < 9){
+        switch(this.id) {
+            case '1':
+                console.log('Clicked ' + 1);
+                firstNumber += '1';
+                firstNumber = Number(firstNumber);
+                resultDisplay.innerHTML = firstNumber;
+                break;
+            case '2':
+                console.log('Clicked ' + 2);
+                firstNumber += '2';
+                firstNumber = Number(firstNumber);
+                resultDisplay.innerHTML = firstNumber;
+                break;
+            case '3':
+                console.log('Clicked ' + 3);
+                firstNumber += '3';
+                firstNumber = Number(firstNumber);
+                resultDisplay.innerHTML = firstNumber;
+                break;
+            case '4':
+                console.log('Clicked ' + 4);
+                firstNumber += '4';
+                firstNumber = Number(firstNumber);
+                resultDisplay.innerHTML = firstNumber;
+                break;
+            case '5':
+                console.log('Clicked ' + 5);
+                firstNumber += '5';
+                firstNumber = Number(firstNumber);
+                resultDisplay.innerHTML = firstNumber;
+                break;
+            case '6':
+                console.log('Clicked ' + 6);
+                firstNumber += '6';
+                firstNumber = Number(firstNumber);
+                resultDisplay.innerHTML = firstNumber;
+                break;
+            case '7':
+                console.log('Clicked ' + 7);
+                firstNumber += '7';
+                firstNumber = Number(firstNumber);
+                resultDisplay.innerHTML = firstNumber;
+                break;
+            case '8':
+                console.log('Clicked ' + 8);
+                firstNumber += '8';
+                firstNumber = Number(firstNumber);
+                resultDisplay.innerHTML = firstNumber;
+                break;
+            case '9':
+                console.log('Clicked ' + 9);
+                firstNumber += '9';
+                firstNumber = Number(firstNumber);
+                resultDisplay.innerHTML = firstNumber;
+                break;
+        };
+    }else {
+        alert('Maximum number length reached');
+    };
+};
 
-function divide(firstNumber,secondNumber) {
-    return firstNumber / secondNumber;
-}
 
-console.log(add(1, 2));
-console.log(subtract(1, 2));
-console.log(multiply(1, 2));
-console.log(divide(1, 2));
+function operate(firstNum, secondNum, operator) {
+    /* Add this function to the = sign) */
+};
